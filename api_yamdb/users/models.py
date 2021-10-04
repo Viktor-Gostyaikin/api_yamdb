ce76e7b96c71ec8ec367c2c96d19f58e6bc40957
Victor_Gostyaikin <gostyaikin@yandex.ru>
2021-10-04 18:36:07 +0300

    Добавлено приложение Users с моделью

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+    SUPERUSER = 'superuser'
+    ROLE = (
+        (USER, 'Пользователь'),
+        (MODERATOR, 'Модератор'),
+        (ADMIN, 'Администратор'),
+        (SUPERUSER, 'Суперпользователь'),
+    )
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+    role = models.CharField(
+        'Роль', choices=ROLE, default=USER
+    )
