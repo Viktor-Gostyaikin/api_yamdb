2b51bb3264ed62543d9e2b7b3ca8ce355c5122ab
Victor_Gostyaikin <gostyaikin@yandex.ru>
2021-10-05 00:09:03 +0300

    набросал заготовки для моделей, подшлифовал models.Review

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,17 +1,27 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-#from .users.models import User
 
 User = get_user_model()
 
-SCORE = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# SCORE = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+
+class Title(models.Model):
+    name = None
+    year = None
+    category = None
 
 
 class Review(models.Model):
-    text = models.CharField(max_lengt=10000)
+    title_id = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    text = models.CharField(max_length=10000)
     author = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='reviews')
-    score = models.CharField(max_lengt=2, choices=SCORE)
+    score = models.SmallIntegerField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
 
 
@@ -22,8 +32,23 @@ class Comment(models.Model):
         related_name='comments',
         db_index=True
     )
-    text = models.TextField(max_lengt=10000)
+    text = models.TextField(max_length=10000)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='comments')
     pub_date = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Category(models.Model):
+    name = None
+    slug = None
+
+
+class Genre(models.Model):
+    name = None
+    slug = None
+
+
+class Genre_Title(models.Model):
+    title_id = None
+    genre_id = None
