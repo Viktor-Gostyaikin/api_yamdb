a6e0b5db1367db0951d431c99715560a66091179
Catherine <catrain2020@gmail.com>
2021-10-04 21:33:55 +0300

    Added views ReviewViewSe, CommentViewSet. reviews/urls.py: added routers

--- api_yamdb/reviews/views.py
+++ api_yamdb/reviews/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
 
 from .models import Title
-from .serializers import ReviewSerializer
+from .serializers import ReviewSerializer, CommentSerializer
 from .permissions import AuthorOrReadOnly
 
 
@@ -29,4 +29,18 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    pass
+    serializer_class = CommentSerializer
+    permission_classes = (AuthorOrReadOnly,)
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, id=self.kwargs['title_id'])
+        review = title.reviews.all()
+        return review
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user, title=get_object_or_404(
+                Title, id=self.kwargs['title_id']
+            )
+        )
