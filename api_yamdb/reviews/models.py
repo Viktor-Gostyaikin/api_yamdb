0d9b48f108e56f44d8003157149a9e451ef994a0
dan1k-aby <danik.aby@mail.ru>
2021-10-06 19:53:41 +0600

    model genre, title, category, genre_title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,10 +2,50 @@ from django.db import models
 
 from users.models import User
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=256, unique=True,
+                            verbose_name='категория')
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+    
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256, unique=True, verbose_name='Жанр')
+    slug = models.SlugField(max_length=56, unique=True)
+    
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+    
+    def __str__(self):
+        return self.name
+
+
 class Title(models.Model):
-    name = None
-    year = None
-    category = None
+    name = models.CharField(max_length=256, verbose_name='произведения')
+    year = models.IntegerField(
+        verbose_name="Год издания", db_index=True, default='2021')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,
+                                 blank=True, null=True, related_name="titles")
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+    
+    def __str__(self):
+        return self.name
+
+class Genre_Title(models.Model):
+    title_id = models.ForeignKey(Title, on_delete=models.CASCADE,
+                                 related_name="titles", default=1)
+    genre_id = models.ForeignKey(Genre, related_name="ganre", on_delete=models.CASCADE, default=1)
 
 
 class Review(models.Model):
@@ -50,26 +90,3 @@ class Comment(models.Model):
         auto_now_add=True,
         db_index=True
     )
-
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=256, unique=True,
-                            verbose_name='категория')
-    slug = models.SlugField(max_length=50, unique=True)
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-    
-    def __str__(self):
-        return self.name
-
-
-class Genre(models.Model):
-    name = None
-    slug = None
-
-
-class Genre_Title(models.Model):
-    title_id = None
-    genre_id = None
